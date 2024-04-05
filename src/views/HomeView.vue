069fd8f37594471398eb9324ff458fe0e41e86a8
Priyanshi Gupta <priyanshialambagh@gmail.com>
2024-04-05 12:58:56 +0530

    added till section 6

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 <template>
   <div class="home">
@@ -51,10 +52,87 @@ import {reactive,computed,watch} from 'vue'
     counterData.count-=amount
   }
 
+=======
+<template>
+  <div class="home">
+    
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}:</h3>
+
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+/*
+  imports
+*/
+
+  import { reactive, computed, watch, onMounted } from 'vue'
+
+/*
+  app title
+*/
+
+  const appTitle = 'My Ok Counter App'
+
+  onMounted(() => {
+    console.log('Do stuff related to App Title')
+  })
+
+/*
+  counter
+*/
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
+
+  watch(() => counterData.count, (newCount) => {
+    if (newCount === 20) {
+      alert('Way to go! You made it to 20!!')
+    }
+  })
+
+  const oddOrEven = computed(() => {
+    if (counterData.count % 2 === 0) return 'even'
+    return 'odd'
+  })
+
+  const increaseCounter = (amount, e) => {
+    counterData.count += amount
+  }
+
+  const decreaseCounter = amount => {
+    counterData.count -= amount
+  }
+
+  onMounted(() => {
+    console.log('Do stuff related to Counter')
+  })
+
+>>>>>>> c9cf6a3 (added till section 6)
 </script>
 
 <!--
 <script>
+<<<<<<< HEAD
 export default{
   data(){
     return{
@@ -71,11 +149,36 @@ export default{
     count(newCount,oldCount){
       if(newCount==20) alert ('asdfasd')
     }
+=======
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    myComputedProperty() {
+      // perform logic based on a data property
+      return 'my result'
+    }
+  },
+  watch: {
+    count(newCount, oldCount) {
+      if (newCount == 20) alert('asdfasd')
+    }
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  unmounted() {
+    console.log('unmounted')
+>>>>>>> c9cf6a3 (added till section 6)
   }
 }
 </script>
 -->
 
+<<<<<<< HEAD
 <!--
 <script>
 import {ref} from 'vue'
@@ -135,4 +238,18 @@ export default{
   margin-top:60px;
 }
 
+=======
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+>>>>>>> c9cf6a3 (added till section 6)
 </style>

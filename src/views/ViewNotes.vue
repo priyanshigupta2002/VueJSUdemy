292b61d2b8d9f65bd41a3dc0b09910089e82b57a
Priyanshi Gupta <priyanshialambagh@gmail.com>
2024-04-12 18:19:04 +0530

    till section15

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,35 +1,88 @@
 <template>
-    <div class="notes">
-  
-      <div class="card has-background-success-dark p-4 mb-5">
-        <div class="field">
-          <div class="control">
-            <textarea
-              class="textarea"
-              placeholder="Add a new note"
-            />
-          </div>
-        </div>
-  
-        <div class="field is-grouped is-grouped-right">
-          <div class="control">
-            <button class="button is-link has-background-success">
-              Add New Note
-            </button>
-          </div>
+  <div class="notes">
+
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
-  
-      <div v-for="i in 3" :key=i class="card mb-4">
-        <div class="card-content">
-          <div class="content">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.
-          </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
+            Add New Note
+          </button>
         </div>
-        <footer class="card-footer">
-          <a href="#" class="card-footer-item">Edit</a>
-          <a href="#" class="card-footer-item">Delete</a>
-        </footer>
       </div>
     </div>
-  </template>
+
+    <Note
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+      @deleteClicked="deleteNote"
+    />
+
+  </div>
+</template>
+
+<script setup>
+
+/*
+  imports
+*/
+
+  import { ref } from 'vue'
+  import Note from '@/components/Notes/Note.vue'
+
+/*
+  notes
+*/
+
+  const newNote = ref('')
+  const newNoteRef = ref(null)
+
+  const notes = ref([
+    {
+      id: 'id1',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+    },
+    {
+      id: 'id2',
+      content: 'This is a shorter note! Woo!'
+    }
+  ])
+
+  const addNote = () => {
+    let currentDate = new Date().getTime(),
+        id = currentDate.toString()
+
+    let note = {
+      id,
+      content: newNote.value
+    }
+
+    notes.value.unshift(note)
+    newNote.value = ''
+    newNoteRef.value.focus()
+  }
+
+/*
+  delete note
+*/
+
+  const deleteNote = idToDelete => {
+    notes.value = notes.value.filter(note => { return note.id !== idToDelete })
+  }
+
+</script>

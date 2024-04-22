d4ae4cc7177c31a23f0fc538786882ab1b28c021
Priyanshi Gupta <priyanshialambagh@gmail.com>
2024-04-22 17:33:56 +0530

    till section17

--- src/components/Notes/AddEditNote.vue
+++ src/components/Notes/AddEditNote.vue
@@ -1,40 +1,50 @@
 <template>
-    <div
-      class="card p-4 mb-5"
-      :class="`has-background-${bgColor}-dark`"
+  <div
+    class="card p-4 mb-5"
+    :class="`has-background-${ bgColor }-dark`"
+  >
+    <label
+      v-if="label"
+      class="label has-text-white"
     >
-      <label
-        v-if="label"
-        class="label has-text-white"
-      >
-        {{ label }}
-      </label>
-  
-      <div class="field">
-        <div class="control">
-          <textarea
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
-            class="textarea"
-            :placeholder="placeholder"
-            ref="textareaRef"
-            maxlength="100"
-            v-autofocus
-          />
-        </div>
-      </div>
-  
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <slot name="buttons" />
-        </div>
+      {{ label }}
+    </label>
+
+    <div class="field">
+      <div class="control">
+        <textarea
+          v-model="modelValue"
+          @input="$emit('update:modelValue', modelValue)"
+          class="textarea"
+          :placeholder="placeholder"
+          ref="textareaRef"
+          maxlength="100"
+          v-autofocus
+        />
       </div>
     </div>
-  </template>
-  
-  <script setup>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+/*
+  imports
+*/
+
   import { ref } from 'vue'
-  
+  import { vAutofocus } from '@/directives/vAutofocus'
+
+/*
+  props
+*/
+
   const props = defineProps({
     modelValue: {
       type: String,
@@ -52,16 +62,25 @@
       type: String
     }
   })
-  
+
+/*
+  emits
+*/
+
   const emit = defineEmits(['update:modelValue'])
-  
+
+/*
+  focus textarea
+*/
+
   const textareaRef = ref(null)
-  
+
   const focusTextarea = () => {
     textareaRef.value.focus()
   }
-  
+
   defineExpose({
     focusTextarea
   })
-  </script>
+
+</script>
